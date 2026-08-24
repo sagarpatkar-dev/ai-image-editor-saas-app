aaeb35f0af68984c22bba2f571c77be07332294c
Sagar Patkar <sagarpatkar.dev@gmail.com>
2026-08-25 01:15:18 +0530

    Add generated Prisma client files.

--- src/server/better-auth/client.ts
+++ src/server/better-auth/client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react";
+import {env} from "~/env";
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+    baseURL: env.BETTER_AUTH_URL,
+});
 
 export type Session = typeof authClient.$Infer.Session;
